bf2c4a6d2e963ea7e443bb63f1749492b4333364
7Nic <gabriel.nicolau97@hotmail.com>
2021-12-04 15:20:19 -0300

    removing docker

--- src/server/app.py
+++ src/server/app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, request, jsonify
 from pathlib import Path
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST'])
 def hello():
-    # return "Hello Nickson"
-    response = jsonify({'similarity': 'Similarity here'})
+    s1 = request.form['sentence1']
+    s2 = request.form['sentence2']
+    print(s1, s2)
+    response = jsonify({'similarity': '90%'})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+if __name__ == "__main__":
+    app.run(debug=True)
