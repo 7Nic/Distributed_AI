05b7f40c545e299e200f271bf686c35f77e4cee0
M-A-rs <araujo_m_@hotmail.com>
2021-12-19 02:20:42 -0300

    Refactor Block proof of work and hashing

--- blockchain/block.py
+++ blockchain/block.py
@@ -1,23 +1,36 @@
 from datetime import datetime
 from typing import Any
 
-from validation import to_hash
+from validation import to_hash, is_valid_proof
 
 class Block:
-    def __init__(self, data: Any, previous_hash, proof: int):
+    def __init__(self, index: int, data: Any, previous_hash, proof: int):
         """
         Classe definindo um bloco de um Blockchain.
 
         Args:
+            index (int): índice do bloco no Blockchain
             data (Any): dados a serem armazenados no bloco.
             previous_hash: hash do bloco anterior.
             proof (int): valor único utilizado para o Proof of Work dos blocos.
         """        
+        self._index = index
         self._data = data
         self._timestamp = datetime.utcnow()
         self._previous_hash = previous_hash
-        self._proof = proof
-        self._hash = to_hash(str(self))
+        self._nounce = 0
+        self._hash = self.proof_of_work()
     
+    def proof_of_work(self):
+        current_hash = to_hash(str(self))
+        while not is_valid_proof(current_hash):
+            self._nounce += 1
+            current_hash = to_hash(str(self))
+        return current_hash
+
+    @property
+    def hash(self):
+        return self._hash
+
     def __str__(self):
-        return f"{self._data} {self._timestamp} {self._previous_hash} {self._proof}"
+        return f"{self._index} {self._data} {self._timestamp} {self._previous_hash} {self._nounce}"
