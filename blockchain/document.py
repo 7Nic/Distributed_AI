3492433f5905fc946e0117652bb390a459095d26
M-A-rs <araujo_m_@hotmail.com>
2021-12-20 07:00:48 -0300

    Add Document class

---
+++ blockchain/document.py
@@ -0,0 +1,24 @@
+class Document:
+    def __init__(self, title: str, body: str):
+        """
+        Classe que define um documento de texto.
+
+        Args
+            title (str): título do documento.
+            body (str): corpo do texto.
+        """
+        self._title = title
+        self._body = body
+    
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Document):
+            return self._title == other._title and self._body == other._body
+        return False
+
+    @property
+    def title(self):
+        return self._title
+    
+    @property
+    def body(self):
+        return self._body
