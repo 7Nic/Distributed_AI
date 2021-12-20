cf85b205d0b329ebe08bdbf02bf48625a95be5c0
M-A-rs <araujo_m_@hotmail.com>
2021-12-20 07:12:23 -0300

    Refactor similarity to use Document instead of strings

--- blockchain/similarity.py
+++ blockchain/similarity.py
@@ -6,54 +6,58 @@ import tensorflow_text
 from scipy import spatial
 from typing import Iterable, Tuple
 
+from document import Document
+
 model = hub.load("https://tfhub.dev/google/universal-sentence-encoder-multilingual/3")
 
 class Similarity:
-    def __init__(self, first_string: str, second_string: str):
+    def __init__(self, first_document: Document, second_document: Document):
         """
         Classe para armazenar informações sobre a similaridade
-        entre textos.
+        entre documentos.
 
         Args:
-            first_string, second_string (str): textos em que se 
-            deve aplicar o cálculo de similaridade semântica.
+            first_document, second_document (Documento): instâncias
+            de documentos em que se deve aplicar o cálculo de similaridade 
+            semântica no corpo do texto.
         """
-        self._first_string = first_string
-        self._second_string = second_string
-        self._similarity = compute_similarity(first_string, second_string)
+        self._first_document = first_document
+        self._second_document = second_document
+        self._similarity = compute_similarity(first_document.body, second_document.body)
     
     @property
     def similarity(self) -> float:
         return self._similarity
     
     @property
-    def texts(self) -> Tuple[str]:
-        return (self._first_string, self._second_string)
+    def documents(self) -> Tuple[Document]:
+        return (self._first_document, self._second_document)
 
     def __str__(self) -> str:
-        return f"Similaridade({self._first_string}, {self._second_string}) = {self._similarity:.2f}"
+        return f"Similaridade({self._first_document.title}, {self._second_document.title}) = {self._similarity:.2f}"
     
     def __eq__(self, other) -> bool:
         if isinstance(other, Similarity):
-            return self._first_string == other._first_string and self._second_string == other._second_string
+            return self._first_document == other._first_document and self._second_document == other._second_document
         return False
 
-    def same_text(self, other: Iterable[str]) -> bool:
+    def same_document(self, other: Iterable[Document]) -> bool:
         """
-        Dado um iterável contendo strings, verifica se o iterável possui os mesmos textos
-        que a instância da classe de Similaridade.
+        Dado um iterável de documentos, verifica se este iterável possui documentos equivalentes
+        ao da instância da classe Similarity.
 
         Args:
-            other (Iterable[str]): iterável contendo strings.
+            other (Iterable[Document]): iterável contendo documentos.
         
         Returns:
             (bool): True se o iterável possuir o mesmo conjunto de textos que a instância
             da classe Similaridade, False do contrário.
         """
-        if len(self.texts) != len(other):
+        if len(self.documents) != len(other.documents):
             return False
                 
-        return set(self.texts) == set(other)
+        return set([document.title for document in self.documents]) == set([document.title for document in other.documents]) \
+            and set([document.body for document in self.documents]) == set([document.body for document in other.documents])
 
 def compute_similarity(first_string: str, second_string: str) -> float:
     """
